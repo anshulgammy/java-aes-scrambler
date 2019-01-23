c461c45172069e9d87e9a299ef5f990ebd219357
Anshul Gautam <anshulgammy@outlook.com>
2019-01-23 22:22:48 +0530

    done file changes

--- src/main/java/com/scrambler/aes/util/CommonUtil.java
+++ src/main/java/com/scrambler/aes/util/CommonUtil.java
@@ -1,26 +1,25 @@
 package com.scrambler.aes.util;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommonUtil {
 	
-	public static final ClassLoader CLASS_LOADER = ClassLoader.getSystemClassLoader();
+	public static final ClassLoader CLASS_LOADER = Thread.currentThread().getContextClassLoader();
 	
 	public static String[] readFromFile(final String fileName) {
 		final List<String> algorithms = new ArrayList<String>();
 		BufferedReader bufferedReader = null;
 		final String fileNameToRead = fileName + ".txt";
 		try {
-			final URL urlToFile = CLASS_LOADER.getResource(fileNameToRead);
-			System.out.println(URLDecoder.decode(urlToFile.getFile()));
-			final FileReader fileReader = new FileReader(URLDecoder.decode(urlToFile.getFile(), AesConstants.UTF_8));
-			bufferedReader = new BufferedReader(fileReader);
+			final InputStream inputStream = CLASS_LOADER.getResourceAsStream(fileNameToRead);
+		    final InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+		    bufferedReader = new BufferedReader(streamReader);
 			String str = null;
 			while ((str = bufferedReader.readLine()) != null) {
 				algorithms.add(str);
